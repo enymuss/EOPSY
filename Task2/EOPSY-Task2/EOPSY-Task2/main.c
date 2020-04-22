4f8b623f0de29b2033556870f6226d57d126a94f
Richard Szczerba <richardszczerba@gmail.com>
2020-04-23 01:14:27 +0200

    combined version

--- Task2/EOPSY-Task2/EOPSY-Task2/main.c
+++ Task2/EOPSY-Task2/EOPSY-Task2/main.c
@@ -6,15 +6,13 @@
 //  Copyright © 2020 Richard Szczerba. All rights reserved.
 //
 
-// test synchronization mechanisms and signals.
-// Use the following system functions: fork(), wait(), signal() or sigaction() and kill().
-
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include "stdbool.h"
 
 #define NUM_CHILD 3
 
@@ -26,6 +24,10 @@
 
 void child(void);
 void parent(int pid);
+void ignoreHandler(int sig);
+void terminationHandler(int sig);
+
+bool keyboardInterrupt = false;
 
 int main(int argc, const char * argv[]) {
     
@@ -35,12 +37,20 @@ int main(int argc, const char * argv[]) {
     
     for (int i = 0; i<NUM_CHILD; i++) {
         forkValue = fork();
+        if (keyboardInterrupt) {
+            forkValue = -1;
+        }
         sleep(1);
         
         switch (forkValue) {
             case -1:
                 // error
-                printf("parent[%d]: Failed to create child process", getpid());
+                if (keyboardInterrupt) {
+                    printf("parent[%d]: Interrupt of creation process", getpid());
+                } else {
+                    printf("parent[%d]: Failed to create child process", getpid());
+                }
+                
                 // kill child process
                 for (int i = 0; i<NUM_CHILD; i++) {
                     if (childArray[i] > 0) {
@@ -55,6 +65,22 @@ int main(int argc, const char * argv[]) {
                 
             default:
                 childArray[i] = forkValue;
+                
+                struct sigaction new_sa;
+                struct sigaction old_sa;
+                sigfillset(&new_sa.sa_mask);
+                new_sa.sa_handler = SIG_IGN;
+                new_sa.sa_flags = 0;
+
+                for (int j = 0; j<32; j++) {
+                    if (j != SIGCHLD) {
+                        if (sigaction(j, &new_sa, &old_sa) == 0 && old_sa.sa_handler != SIG_IGN)
+                        {
+                            new_sa.sa_handler = ignoreHandler;
+                            sigaction(SIGINT, &new_sa, 0);
+                        }
+                    }
+                }
                 wait(NULL);
                 break;
         }
@@ -78,13 +104,35 @@ int main(int argc, const char * argv[]) {
         printf("parent[%d]: Count exit codes: %d\n", getpid(), countTerminations);
     }
     
+    // restore handlers.
+    for (int i=0; i<32; i++) {
+        signal(i, SIG_DFL);
+    }
+    
     return 0;
 }
 
 
 void child(void) {
+    struct sigaction new_sa;
+    sigfillset(&new_sa.sa_mask);
+    new_sa.sa_flags = 0;
+
+    new_sa.sa_handler = terminationHandler;
+    sigaction(SIGTERM, &new_sa, NULL);
+    sigaction(SIGTERM, NULL, NULL);
+    
     printf("child[%d]: Parent PID: %d\n", getpid(), getppid());
-    sleep(5);
+    sleep(10);
     printf("child[%d]: Execution completed\n", getpid());
     // exit(0)
+}
+
+void ignoreHandler(int sig) {
+    printf("You've used control-c. Info int: %d\n", sig);
+    keyboardInterrupt = true;
+}
+
+void terminationHandler(int sig) {
+    printf("This process is terminated");
 }
